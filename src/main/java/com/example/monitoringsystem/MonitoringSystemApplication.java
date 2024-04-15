ddad028abe10555452aedf7effbf3a7c343f858d
YuriPetukhov <petuchowjuri@yahoo.de>
2024-04-15 10:00:24 +0300

    monitoring

--- src/main/java/com/example/monitoringsystem/MonitoringSystemApplication.java
+++ src/main/java/com/example/monitoringsystem/MonitoringSystemApplication.java
@@ -3,10 +3,8 @@ package com.example.monitoringsystem;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.example.monitoringsystem.mapper")
 @OpenAPIDefinition
 public class MonitoringSystemApplication {
 
